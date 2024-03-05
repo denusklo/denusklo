1d882934eb074b40830b6a03b82a6f2340dc2291
denusklo <denusklo@gmail.com>
2024-03-05 23:11:55 +0800

    latest commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import preact from "@astrojs/preact";
-import react from '@astrojs/react';
 import vue from "@astrojs/vue";
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), vue(), react()]
+  integrations: [tailwind(), vue()]
 });
